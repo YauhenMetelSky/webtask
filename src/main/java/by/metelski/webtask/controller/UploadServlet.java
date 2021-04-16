0a8f956941d019642a6e6a8a303508801db3b2b9
Yauheni <lynxfortest@gmail.com>
2021-04-16 09:13:48 +0300

    added upload servlet

---
+++ src/main/java/by/metelski/webtask/controller/UploadServlet.java
@@ -0,0 +1,69 @@
+package by.metelski.webtask.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import by.metelski.webtask.command.SessionAttribute;
+
+@WebServlet(name = "upload_servlet", urlPatterns = { "/upload" })
+@MultipartConfig(fileSizeThreshold = 1024 *1024,
+                 maxFileSize = 1024 * 1024 *5,
+                 maxRequestSize = 1024 * 1024 *5 *5)
+public class UploadServlet extends HttpServlet {
+	private static final Logger logger = LogManager.getLogger();
+
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		logger.log(Level.DEBUG, "Upload servlet");
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		String uploadFileDir = "E:\\Down";
+		File fileSaveDir = new File(uploadFileDir);
+		if(!fileSaveDir.exists()) {
+			fileSaveDir.mkdirs();
+		}
+		request.getParts().stream().forEach(part ->{
+			try {
+				String path = part.getSubmittedFileName();
+				part.write(uploadFileDir + File.separator + path);
+				request.setAttribute("upload_result","upload successfully");
+			} catch (IOException e) {
+				logger.log(Level.ERROR, "UPS... Cap we have a problem");
+				request.setAttribute("upload_result","upload filed");
+				// TODO: handle exception
+			}
+		});
+		String page = (String) session.getAttribute(SessionAttribute.CURRENT_PAGE);
+		request.getRequestDispatcher(page).forward(request,response);
+		// TODO Auto-generated method stub
+		super.doPost(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	
+
+}
---
+++ src/main/webapp/jsp/header.jsp
@@ -0,0 +1,45 @@
+<%@page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"
+	language="java"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<fmt:setLocale value="${locale}" scope="session" />
+<fmt:setBundle basename="pagecontent" />
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta name="viewport"
+	content="width=device-width, initial-scale=1,  shrink-to-fit=no charset=utf-8" />
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+	integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+	crossorigin="anonymous"></script>
+<script
+	src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+	integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+	crossorigin="anonymous"></script>
+<link rel="stylesheet"
+	href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+	integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+	crossorigin="anonymous">
+<link rel="stylesheet" href="css/signin.css">
+<title>Doctor pro</title>
+</head>
+<body>
+<form action="controller" method="post">
+<div>
+					<div class="dropdown">
+						<button class="btn btn-primary dropdown-toggle" type="button"	id="dropdownMenuButton" data-toggle="dropdown"
+							aria-haspopup="true" aria-expanded="false"><fmt:message key="label.language"/></button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<button class="dropdown-item" type="submit" name="language"
+								value="en_US">English</button>
+							<button class="dropdown-item" type="submit" name="language"
+								value="ru_RU">Russian</button>
+							<input type="hidden" name="command" value="set_locale">
+						</div>
+					</div>
+				</form>
+</div>
+</body>
+</html>
